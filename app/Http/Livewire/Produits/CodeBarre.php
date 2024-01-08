e779bbb952a3e8ac3551aa452f3fd0ded7f87d82
Idrissa Ndiouck <indiouck04@yahoo.fr>
2024-01-08 13:03:15 +0000

    Générer le code barre ou le code QR

--- app/Http/Livewire/Produits/CodeBarre.php
+++ app/Http/Livewire/Produits/CodeBarre.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Produits;
 use App\Models\CodeBarre as ModelsCodeBarre;
 use Livewire\Component;
 use App\Models\Produit;
+use Illuminate\Support\Facades\Storage;
 use Milon\Barcode\DNS1D;
 use Milon\Barcode\DNS2D;
 
@@ -40,29 +41,6 @@ class CodeBarre extends Component
         $this->search = ''; // Effacer la recherche
     }
 
-    // public function generateBarcode()
-    // {
-    //     // Initialiser la propriété
-    //     $this->imagePath = null;
-
-    //     // Vérifier si un produit est sélectionné
-    //     if ($this->selectedProduct) {
-    //         // Générer le code-barres à partir de la valeur du produit
-    //         $barcode = new DNS1D();
-    //         $barcode->setStorPath(storage_path('app/public/barcodes/'));
-    //         $imagePath = $barcode->getBarcodePNGPath($this->selectedProduct->code, 'C39', 3, 33);
-
-    //         // Sauvegarder le chemin de l'image dans la base de données
-    //         $codeBarre = CodeBarre::create([
-    //             'barcode_chemin_img' => $imagePath,
-    //             'valeur_code_barre' => $this->selectedProduct->code_produit,
-    //             'type_code_barre' => 'C39', // Vous pouvez ajuster cela en fonction du type réel de code-barres
-    //         ]);
-
-    //         // Attribuer la valeur à la propriété
-    //         $this->imagePath = $imagePath;
-    //     }
-    // }
     public function generateBarcode($type)
     {
         // Initialiser la propriété
@@ -71,97 +49,46 @@ class CodeBarre extends Component
         // Vérifier si un produit est sélectionné
         if ($this->selectedProduct) {
             // Générer le code-barres à partir de la valeur du produit
-            $code = $this->selectedProduct->code;
+            $code = $this->selectedProduct->code_produit;
 
-            // Utiliser le type de code-barres spécifié
+            // Instancier la classe correspondante en fonction du type
             switch ($type) {
                 case 'C39':
+                case 'EAN13':
+                case 'PHARMA':
+                case 'C128':
+                case 'C39E+':
                     $barcode = new DNS1D();
-                    $barcode->setStorPath(storage_path('app/public/barcodes/'));
-                    $barcodeImage = $barcode->getBarcodePNG($code, $type, 3, 33);
                     break;
-
                 case 'QRCODE':
                     $barcode = new DNS2D();
-                    $barcode->setStorPath(storage_path('app/public/barcodes/'));
-                    $barcodeImage = $barcode->getBarcodePNG($code, $type);
                     break;
-
-                case 'PDF417':
-                    $barcode = new DNS2D();
-                    $barcode->setStorPath(storage_path('app/public/barcodes/'));
-                    $barcodeImage = $barcode->getBarcodePNG($code, $type);
-                    break;
-
-                    // Ajoutez d'autres types de code-barres au besoin
-
                 default:
-                    // Type par défaut
                     $barcode = new DNS1D();
-                    $barcodeImage = $barcode->getBarcodePNG($code, 'C39', 3, 33);
+                    $type = 'C39'; // Type par défaut
                     break;
             }
 
-            // Sauvegarder le chemin de l'image dans la base de données
+            if (method_exists($barcode, 'setStorPath')) {
+                $barcode->setStorPath(storage_path('app/public/codebarres/'));
+            }
+
+            // Génération du code-barres en fonction du type
+            if ($type === 'QRCODE' && method_exists($barcode, 'getBarcodePNG')) {
+                $barcodeImage = $barcode->getBarcodePNG($code, $type);
+            } else {
+                $barcodeImage = $barcode->getBarcodePNG($code, $type, 1, 55);
+            }
+
+            // Sauvegarde du chemin de l'image dans la base de données
             $codeBarre = ModelsCodeBarre::create([
                 'barcode_chemin_img' => $barcodeImage,
                 'valeur_code_barre' => $this->selectedProduct->code_produit,
                 'type_code_barre' => $type,
             ]);
 
-            // Attribuer la valeur à la propriété
+            // Attribution de la valeur à la propriété
             $this->imagePath = $barcodeImage;
         }
     }
-    // public function generateBarcode($type)
-    // {
-    //     // Initialiser la propriété
-    //     $this->imagePath = null;
-
-    //     // Vérifier si un produit est sélectionné
-    //     if ($this->selectedProduct) {
-    //         // Générer le code-barres à partir de la valeur du produit
-    //         $code = $this->selectedProduct->code_produit;
-
-    //         // Utiliser le type de code-barres spécifié
-    //         switch ($type) {
-    //             case 'C39':
-    //                 $barcode = new DNS1D();
-    //                 $barcode->setStorPath(storage_path('app/public/barcodes/'));
-    //                 $barcodeImage = $barcode->getBarcodePNG($code, $type, 3, 33);
-    //                 break;
-
-    //             case 'QRCODE':
-    //                 $barcode = new DNS2D();
-    //                 $barcodeImage = $barcode->getBarcodePNG($code, $type);
-    //                 break;
-
-    //             case 'PDF417':
-    //                 $barcode = new DNS2D();
-    //                 $barcodeImage = $barcode->getBarcodePNG($code, $type);
-    //                 break;
-
-    //                 // Ajoutez d'autres types de code-barres au besoin
-
-    //             default:
-    //                 // Type par défaut
-    //                 $barcode = new DNS1D();
-    //                 $barcodeImage = $barcode->getBarcodePNG($code, 'C39', 3, 33);
-    //                 break;
-    //         }
-    //         // Enregistrer l'image générée dans le système de fichiers de Laravel
-    //         $fileName = 'barcode_' . $this->selectedProduct->nomProduit . time() . '.png';
-    //         Storage::disk('public')->put('codebarres/' . $fileName, $barcodeImage);
-
-    //         // Sauvegarder le chemin de l'image dans la base de données
-    //         $codeBarre = ModelsCodeBarre::create([
-    //             'barcode_chemin_img' => 'codebarres/' . $fileName,
-    //             'valeur_code_barre' => $this->selectedProduct->code_produit,
-    //             'type_code_barre' => $type,
-    //         ]);
-
-    //         // Attribuer la valeur à la propriété
-    //         $this->imagePath = 'codebarres/' . $fileName;
-    //     }
-    // }
 }
